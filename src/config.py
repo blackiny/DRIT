6c17bd000093718a3379f62fc9433581fa41b742
Sun Ke <ke.sun@ntu.edu.sg>
2021-01-26 19:51:34 +0800

    add cfg funcs

--- src/config.py
+++ src/config.py
@@ -31,3 +31,39 @@ __C.TRAIN.ALTITUDE_RANGE = 90
 __C.TRAIN.INCLASS_PAIR_RATIO = 0.3
 __C.TRAIN.MAX_MODEL_PER_CLASS = 2000
 __C.TRAIN.CLASSES = 'chair,table'
+
+
+def _merge_a_into_b(a, b):
+  """Merge config dictionary a into config dictionary b, clobbering the
+  options in b whenever they are also specified in a.
+  """
+  if type(a) is not edict:
+    return
+
+  for k, v in a.items():
+    # a must specify keys that are in b
+    if k not in b.keys():
+      raise KeyError('{} is not a valid config key'.format(k))
+
+      # the types must match, too
+    if type(b[k]) is not type(v):
+      raise ValueError(('Type mismatch ({} vs. {}) ''for config key: {}').format(type(b[k]), type(v), k))
+
+      # recursively merge dicts
+    if type(v) is edict:
+      try:
+        _merge_a_into_b(a[k], b[k])
+      except:
+        print('Error under config key: {}'.format(k))
+        raise
+    else:
+      b[k] = v
+
+
+def cfg_from_file(filename):
+  """Load a config file and merge it into the default options."""
+  import yaml
+  with open(filename, 'r') as f:
+    yaml_cfg = edict(yaml.load(f))
+
+  _merge_a_into_b(yaml_cfg, __C)
